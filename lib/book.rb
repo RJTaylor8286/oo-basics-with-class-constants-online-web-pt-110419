40d5151e2bca2e31c82936a65e8d7673464d9885
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-11 18:54:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
  
-  GENRES = []
+  GENRES = ["Thriller", "Science Fiction", "Romance"]
  
   def initialize(title)
     @title = title
@@ -12,7 +12,6 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
  
-  # create the writer for genre and add the logic for the class constant
   def genre=(genre)
     @genre = genre
     GENRES << genre 
